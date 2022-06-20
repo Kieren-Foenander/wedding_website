9f54e591e84e4a9508e8172ac00af0ab8b161e65
Kieren-Foenander <kfoenander98@gmail.com>
2022-06-20 22:30:32 +1000

    started adding food and drink sections

--- src/Info.js
+++ src/Info.js
@@ -2,23 +2,27 @@ import React from 'react'
 
 export default function Info() {
   return (
-    <div className='container'>
-        <h2>Addresses & Information</h2>
-        <div>
-            <h3>Ceremony</h3>
-            <p>
-                Time: 3pm<br/>
-                Address: 869 Mundoolun Connection Rd, Tambourine QLD 4270<br/>
-                Parking: Onsite at the end of the driveway.
-            </p>
+    <div>
+        <div className='container'>
+            <h2>Addresses & Information</h2>
+        </div>
+        <div className='container'>
+            <div>
+                <h3>Ceremony</h3>
+                <p>
+                    Time: 3pm<br/>
+                    Address: 869 Mundoolun Connection Rd, Tambourine QLD 4270<br/>
+                    Parking: Onsite at the end of the driveway.
+                </p>
 
-            <h3>Reception</h3>
-            <p>
-                Time: After Ceremony <br/>
-                Address: 94 Evergreen Drive, South MacClean 4280 <br/>
-                Parking: Front yard or plenty of street parking
+                <h3>Reception</h3>
+                <p>
+                    Time: After Ceremony <br/>
+                    Address: 94 Evergreen Drive, South MacClean 4280 <br/>
+                    Parking: Front yard or plenty of street parking
 
-            </p>
+                </p>
+            </div>
         </div>
     </div>
   )
--- src/components/Heading.js
+++ src/components/Heading.js
@@ -4,10 +4,9 @@ import { Link } from "react-router-dom"
 
 export default function Heading() {
   return (
-    <div className='heading'>
+    <>
         <h1><Link to="/">The Foenander Wedding</Link></h1>
         <Navbar />
-
-    </div>
+    </>
   )
 }
