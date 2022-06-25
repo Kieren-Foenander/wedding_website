c634a954689490cf58dcc8113f13d7ffc94d7544
Kieren-Foenander <kfoenander98@gmail.com>
2022-06-25 19:56:38 +1000

    added firebase compatibility

--- src/Rsvp.js
+++ src/Rsvp.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { db } from './firebase-config'
+import { collection, getDocs } from 'firebase/firestore'
+
 
 export default function Rsvp() {
+
+  const [guests, setGuests] = useState([])
+
+  useEffect( () => {
+    const guestsCollectionRef = collection(db, "guests")
+    const getGuests = async () => {
+      const data = await getDocs(guestsCollectionRef)
+      setGuests(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    }
+
+    getGuests()
+  }, [])
+
   return (
-    <div>Rsvp</div>
+    <div>
+      {guests.map((guests) =>{
+        return(
+          <div key={guests.id}> 
+            <h1 >Name: {guests.name}</h1>
+            <h1>RSVP: {guests.rsvp.toString()}</h1>
+          </div>
+        )
+      })}
+    </div>
   )
 }
