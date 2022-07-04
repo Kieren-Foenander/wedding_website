6261f0a5e5f22a6f50b63126637340ed7c9cbff9
Kieren-Foenander <kfoenander98@gmail.com>
2022-07-04 12:19:34 +1000

    finished faq and gallery for now

--- src/Questions.js
+++ src/Questions.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function Questions() {
   return (
-    <div>Questions</div>
+   <>
+   <div className='container'>
+      <h2>FAQ</h2>
+   </div>
+   <div className='container'>
+      <h3>Dress Code</h3>
+      <p>Cocktail or formal attire - no white or you’re dead haha.. No seriously, no white ;)</p>
+      <h3>Morning After</h3>
+      <p>Grease, grease and more grease! For those who are staying, we will be having a big breakfast the next morning (please message us if there are any dietary requirements)</p>
+      <h3>Other Questions</h3>
+      <p>If you have any other questions, please message us as well!</p>
+      <h4>Kieren - 0477020777</h4>
+      <h4>Kiah - 0450905617</h4>
+    </div>
+   </>
   )
 }
